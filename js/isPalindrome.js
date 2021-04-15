8dfca5af79a080afeea273b56db350943df441b5
hallegv <56059233+hallegv@users.noreply.github.com>
2021-04-15 14:41:29 -0700

    Now works for strings with "_"

--- js/isPalindrome.js
+++ js/isPalindrome.js
@@ -1,7 +1,7 @@
 // we can iterate w two pointers from front and back 
 var isPalindrome = function(s) {
     // to get only alphanumeric chars & ignore case
-    s = s.replace(/\W/gi,'').toLowerCase();
+    s = s.replace(/[\W_]/gi,'').toLowerCase();
     for (let i = 0; i < s.length/2; i++) {
         if (s[i] !== s[s.length - i - 1]) {
             return false;
@@ -13,6 +13,6 @@ var isPalindrome = function(s) {
 // or we can just use some fun JS helpers and check if the reverse equals the original...
 var isPalindrome = function(s) {
     // to get only alphanumeric chars & ignore case
-    s = s.replace(/\W/gi,'').toLowerCase();
+    s = s.replace(/[\W_]/gi,'').toLowerCase();
     return s === (s.split("").reverse().join(""));
 };
